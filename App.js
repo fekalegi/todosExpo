69b8bbedb2527d009c4002b053c366f65f62ae5d
fekalegi <fekalegi@gmail.com>
2021-05-23 13:42:28 +0700

    Add Alert Function

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { FlatList, StyleSheet, Text, View, ToastAndroid } from 'react-native';
+import { FlatList, StyleSheet, Text, View, ToastAndroid, Alert } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -18,11 +19,28 @@ export default function App() {
     ToastAndroid.show('Request sent successfully!', ToastAndroid.SHORT)
   }
 
+  const submitHandler = (text) => {
+
+    if(text.length > 3 ){
+
+    setTodos((prevTodos) => {
+      return [
+        {text: text, key: Math.random().toString() },
+        ...prevTodos
+      ]
+    })}
+    else{
+      Alert.alert('OOPS!','Todos must be over 3 characters long',[{
+        text: 'understood', onPress: () => console.log('alert closed')
+      }])
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/*Form*/}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList 
             data={todos}
